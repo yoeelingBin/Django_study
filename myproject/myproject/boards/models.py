804b605fa581c97ffe6cf1cbdc4c8621a987deee
颜琪滨 <2535367985@qq.com>
2021-03-31 21:30:40 +0800

    改错，把name换成了subject

--- myproject/myproject/boards/models.py
+++ myproject/myproject/boards/models.py
@@ -12,7 +12,7 @@ class Board(models.Model):
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
