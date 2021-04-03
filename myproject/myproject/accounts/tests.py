b9954da02aabac5202f240f6771d617759268fcb
颜琪滨 <2535367985@qq.com>
2021-04-03 15:49:44 +0800

    Update tests.py

--- myproject/myproject/accounts/tests.py
+++ myproject/myproject/accounts/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from .views import signup
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class SignUpTests(TestCase):
@@ -10,7 +11,7 @@ class SignUpTests(TestCase):
         self.response = self.client.get(url)
 
     def test_signup_status_code(self):
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
@@ -22,3 +23,33 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'john',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirection(self):
+        '''
+        有效的表单提交应该将用户重定向到主页
+        '''
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        '''
+        创建到任意页面的新请求。成功注册后，
+        得到的响应现在应该有一个“user”到它的context。
+        '''
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
