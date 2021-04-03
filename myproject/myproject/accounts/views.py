13eff5e4add045a91b2e6d5369a8d5366ebab1dc
颜琪滨 <2535367985@qq.com>
2021-04-03 16:00:09 +0800

    Update views.py

--- myproject/myproject/accounts/views.py
+++ myproject/myproject/accounts/views.py
@@ -5,7 +5,7 @@ from .forms import SignUpForm
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
