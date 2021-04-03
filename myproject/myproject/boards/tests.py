20cfff116f6dee54672d23f846ebc227c20869a1
颜琪滨 <2535367985@qq.com>
2021-04-03 13:08:15 +0800

    Update tests.py

--- myproject/myproject/boards/tests.py
+++ myproject/myproject/boards/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse, resolve
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
+from .forms import NewTopicForm
 
 
 # Create your tests here.
@@ -58,32 +59,6 @@ class BoardTopicsTests(TestCase):
         self.assertContains(response, 'href="{}"'.format(homepage_url))
         self.assertContains(response, 'href="{}"'.format(new_topic_url))
 
-
-class NewTopicTests(TestCase):
-    def setUp(self):
-        Board.objects.create(name='Django', description='Django board.')
-
-    def test_new_topic_view_success_status_code(self):
-        url = reverse('new_topic', kwargs={'pk':1})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-
-    def test_new_topic_view_not_found_status_code(self):
-        url = reverse('new_topic', kwargs={'pk':99})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
-
-    def test_new_topic_url_resolves_new_topic_view(self):
-        view = resolve('/boards/1/new/')
-        self.assertEquals(view.func, new_topic)
-
-    def test_new_topic_view_contains_link_back_to_board_topics_view(self):
-        new_topic_url = reverse('new_topic', kwargs={'pk':1})
-        board_topics_url = reverse('board_topics', kwargs={'pk':1})
-        response = self.client.get(new_topic_url)
-        self.assertContains(response, 'href="{}"'.format(board_topics_url))
-
-
 class NewTopicTests(TestCase):
     def setUp(self):
         Board.objects.create(name='Django', description='Django board.')
@@ -110,7 +85,9 @@ class NewTopicTests(TestCase):
         '''
         url = reverse('new_topic', kwargs={'pk':1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
@@ -125,3 +102,9 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+
+    def test_contains_form(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
