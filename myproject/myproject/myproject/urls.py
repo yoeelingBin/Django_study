05681c0124c7b1dec75a6beadabcb99d770687e1
颜琪滨 <2535367985@qq.com>
2021-04-04 14:12:18 +0800

    Update urls.py

--- myproject/myproject/myproject/urls.py
+++ myproject/myproject/myproject/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     # url(r'^about/$', views.about, name='about'),
     # url(r'^(?P<username>[\w.@+-]+/$', views.user_profile, name='user_profile'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
