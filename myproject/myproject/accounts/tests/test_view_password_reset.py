aef260d64ab85df8b5cf47a4a2157a4bd461f5be
颜琪滨 <2535367985@qq.com>
2021-04-05 23:58:24 +0800

    Update test_view_password_reset.py

---
+++ myproject/myproject/accounts/tests/test_view_password_reset.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.core import mail
+from django.urls import reverse, resolve
+from django.test import TestCase
+
+
+class PasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        self.response = self.client.get(url)
+    
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, PasswordResetForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain two inputs: csrf and email
+        '''
+        self.assertContains(self.response, 'input', 2)
+        self.assertContains(self.response, 'type="email"', 1)
+
+
+class InvalidPasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        self.response = self.client.post(url, {'email': donotexist@gmail.com'})
+
+    def test_redirection(self):
+        '''
+        Even invalid emails in the database should
+        redirect the user to `password_reset_done` view
+        '''
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+    def test_no_reset_email_sent(self):
+        self.assertEqual(0, len(mail.outbox))
