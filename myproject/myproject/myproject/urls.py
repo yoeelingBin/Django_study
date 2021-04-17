921c2fa9208ace9b45cb53caa960d75990d81698
颜琪滨 <2535367985@qq.com>
2021-04-17 18:24:37 +0800

    Update urls.py

--- myproject/myproject/myproject/urls.py
+++ myproject/myproject/myproject/urls.py
@@ -64,7 +64,7 @@ urlpatterns = [
 
     url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+    url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
     # url(r'^about/company/$', views.about_company, name='about_company'),
     # url(r'^about/author/$', views.about_author, name='about_author'),
