2509312e9abfd43bc8c24b83beb4c39add0090be
颜琪滨 <2535367985@qq.com>
2021-04-17 16:20:09 +0800

    Update views.py

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -17,22 +17,20 @@ class BoardListView(ListView):
     context_object_name = 'boards'
     template_name = 'home.html'
 
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'topics.html'
+    paginate_by = 20
 
-def board_topics(request, pk):
-    board = get_object_or_404(Board, pk=pk)
-    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
-    page = request.GET.get('page', 1)
+    def get_context_data(self, **kwargs):
+        kwargs['board'] = self.board
+        return super().get_context_data(**kwargs)
 
-    paginator = Paginator(queryset, 20)
-
-    try:
-        topics = paginator.page(page)
-    except PageNotAnInteger:
-        topics = paginator.page(1)
-    except EmptyPage:
-        topics = paginator.page(paginator.num_pages)
-
-    return render(request, 'topics.html', {'board': board, 'topics': topics})
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+        queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
+        return queryset
 
 
 @login_required
