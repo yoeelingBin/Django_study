2d7ccfd3f565a1c1c3a99f6d4c62921e889be746
颜琪滨 <2535367985@qq.com>
2021-04-17 15:30:46 +0800

    Update views.py

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Count
 from django.views.generic import UpdateView
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
@@ -68,6 +69,7 @@ def reply_topic(request, pk, topic_pk):
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
 
 
+@method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message', )
@@ -75,9 +77,15 @@ class PostUpdateView(UpdateView):
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
+
     def form_valid(self, form):
         post = form.save(commit=False)
         post.updated_by = self.request.user
         post.updated_at = timezone.now()
         post.save()
-        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
+        return redirect('topic_posts',
+                        pk=post.topic.board.pk,
+                        topic_pk=post.topic.pk)
