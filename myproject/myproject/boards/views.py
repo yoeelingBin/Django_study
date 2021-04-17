cb3c5bab4267454ff989a915cce372611ffb04b9
颜琪滨 <2535367985@qq.com>
2021-04-17 13:58:42 +0800

    Update views.py

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
@@ -23,7 +24,8 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board':board})
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 @login_required
 def new_topic(request, pk):
