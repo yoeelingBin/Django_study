1680ca36fae617cc71b61f9f08173d3fb5345710
颜琪滨 <2535367985@qq.com>
2021-04-17 16:05:53 +0800

    Update views.py

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -5,6 +5,7 @@ from django.db.models import Count
 from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
@@ -19,8 +20,18 @@ class BoardListView(ListView):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    topics = board.topics.order_by('-last_updated').annotate(
-        replies=Count('posts') - 1)
+    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(queryset, 20)
+
+    try:
+        topics = paginator.page(page)
+    except PageNotAnInteger:
+        topics = paginator.page(1)
+    except EmptyPage:
+        topics = paginator.page(paginator.num_pages)
+
     return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
